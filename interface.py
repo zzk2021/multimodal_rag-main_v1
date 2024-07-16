1f78fb602bd8bf2804dad0eec4c2c7554b641423
zzk2021 <zakarichang@outlook.com>
2024-07-16 19:27:36 +0800

    添加接口

--- interface.py
+++ interface.py
@@ -12,6 +12,7 @@ from PIL import Image
 """
 message : json
 """
+# Recived query message Q from users;
 def query(message):
     global retriever_engine_dict
     """
@@ -19,13 +20,17 @@ def query(message):
             question: str
         return: Yes or No
     """
+
     question = message['question']
+    # Query Q in the knowledge base {K} for the results(I, T , P , t );
     _knowledge, img, img_path = complex_analysis(retriever_engine_dict, question, None, None)
+    # Send history Position P to Robot;
     return _knowledge
 
 """
 message : json
 """
+# When Robit arrived, recived image I(i) and Position P(i);
 def arrived_update_and_generate(message):
     global retriever_engine_dict
     """
@@ -48,20 +53,27 @@ def arrived_update_and_generate(message):
             traceback.print_exc()
             _answer = "调用大模型出现错误，错误原因: %s"%(e.__str__())
     else:
+        # 没找到直接插入
         _answer = "yes"
 
     if "yes" in _answer.lower():
         image = Image.open(io.BytesIO(base64.b64decode(base64_str))).convert('RGB')
         file_path = f"storage/knowledgebase/{timestamp}.jpg"
         image.save(file_path)
+        # Get T(i)  describe the sense I by VLM
         _answer = call_LLM("Describe the indoor sense.", image=base64_str,image_path=os.path.join("storage", "cache", "outcome.jpg"), retriever_img_path=img_path)
+        # Insert nodes (I(i),T(i) , P(i), t(i)) to update knowledge base {K}
         update_nodes(file_path, _answer+f". The coordinate is {position}", retriever_engine_dict)
+        # Send to Robot successed to find
         return True
 
     elif "no" in _answer.lower():
         image = Image.open(io.BytesIO(base64.b64decode(base64_str))).convert('RGB')
         file_path = f"storage/knowledgebase/{timestamp}.jpg"
         image.save(file_path)
+        # Get T(i)  describe the sense I by VLM
         _answer = call_LLM("Describe the indoor sense.", image=base64_str, image_path=os.path.join("storage", "cache", "outcome.jpg"), retriever_img_path=img_path)
+        # Insert nodes (I(i),T(i) , P(i), t(i)) to update knowledge base {K}
         update_nodes(file_path, _answer + f". The coordinate is {position}", retriever_engine_dict)
+        # Send to Robot Failed to find
         return False
